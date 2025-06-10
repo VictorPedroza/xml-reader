633aaa1bf3f95df5182e4bf010362b64fcb0d6df
VictorPedroza <Victor242206@gmail.com>
2025-06-10 20:21:26 -0300

    feat: Remove unused XML and Python files; update settings.json with project metadata

--- app/main.py
+++ app/main.py
@@ -1,13 +1,1 @@
-from config.settings import Config
-from folder.main import Folder
-
-settings = Config(__file__)
-
-entry = settings.get_entry
-output = settings.get_output
-backup = settings.get_backup
-
-folder = Folder(entry, output, backup)
-
-for arq in settings.listDir(entry):
-    print(folder.save(arq))
+print("Init project")
